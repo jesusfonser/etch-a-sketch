1987845520e8aa15c7d152faa6faaccf8892e05f
jesusfonser <jesusfonseruma@gmail.com>
2024-08-10 20:44:11 +0200

    Crea cuadrícula 16x16 usando JavaScript y Flexbox
    Se han ajustado las dimensiones de contenedor y añadido padding.

---
+++ script.js
@@ -0,0 +1,10 @@
+let contenedor = document.querySelector("#contenedor");
+
+
+//Medidas de cada cuadrito: 56x56
+
+for (let i = 0; i < 256; i++){
+    let cuadrito = document.createElement("div");
+    cuadrito.classList.add("cuadrito");
+    contenedor.appendChild(cuadrito);
+}
