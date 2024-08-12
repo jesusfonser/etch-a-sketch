eafcf8edfe8232882a6a7caded0782861bce28ce
jesusfonser <jesusfonseruma@gmail.com>
2024-08-12 11:12:25 +0200

    Haz que cada casilla cambie su color al pasar el ratón
    Ha hecho falta cambiar la función randomColorNumber para quitar los
    decimales.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function randomColorNumber () {
-    return Math.random() * 255
+    return Math.floor(Math.random() * 256 + 0);
 }
 
 let contenedor = document.querySelector("#contenedor");
@@ -10,5 +10,13 @@ let contenedor = document.querySelector("#contenedor");
 for (let i = 0; i < 256; i++){
     let cuadrito = document.createElement("div");
     cuadrito.classList.add("cuadrito");
+    cuadrito.addEventListener("mouseover", () => {
+        const colorRandom = [];
+        for (let k = 0; k < 3; k++){
+            colorRandom[k] = randomColorNumber();
+        }
+        cuadrito.style.backgroundColor = `rgb(${colorRandom.toString()})`;
+
+    })
     contenedor.appendChild(cuadrito);
 }
